2f5ad9954b626230db96b7c2c84ae9b0433b26c5
shahariaz <shahariaz.info@gmail.com>
2025-09-16 12:38:03 +0600

    setup dgo client via grpc

---
+++ config/dbConfig.go
@@ -0,0 +1,30 @@
+package configDB
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"github.com/dgraph-io/dgo/v210"
+	"github.com/dgraph-io/dgo/v210/protos/api"
+
+	"github.com/shahariaz/api_dgraph/model"
+)
+
+func InitDgraph() {
+	//	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	conn, err := grpc.NewClient(
+		"localhost:9080",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatal("Failed to connect to Dgraph:", err)
+	}
+
+	dgraphClient := dgo.NewDgraphClient(api.NewDgraphClient(conn))
+
+	if err := model.SetupSchema(dgraphClient); err != nil {
+		log.Fatal(err)
+	}
+}
